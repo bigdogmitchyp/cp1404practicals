7a4c84873cf24b7e58b364b6dd47c6e8b039eea1
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-09-20 20:03:36 +1000

    Fix problems from prac 1, refactor and docstrings

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,17 +1,50 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404 Week 2 Prac
 """
 
-score = float(input("Enter score: "))
-if 100 < score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    elif score > 90:
-        print("Excellent")
+
+def main():
+    """This function will covert scores into grades"""
+    score = collect_user_score()
+    return_grade(score)
+
+
+def return_grade(score):
+    """This function will assign a grade to a score"""
+    if 100 < score < 0:
+        print_invalid()
+    elif score >= 90:
+        print_excellent()
     elif score >= 50:
-        print("Passable")
-    elif score < 50:
-        print("Bad")
+        print_passable()
+    else:
+        print_bad()
+
+
+def print_bad():
+    """This function will print a bad grade"""
+    print("Bad")
+
+
+def print_passable():
+    """This function will print a passable grade"""
+    print("Passable")
+
+
+def print_excellent():
+    """This function will print an excellent grade"""
+    print("Excellent")
+
+
+def print_invalid():
+    """This function will print an error statement"""
+    print("Invalid score")
+
+
+def collect_user_score():
+    """This function will collect users score"""
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
