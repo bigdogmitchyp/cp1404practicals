4f458d9eeb347e61e4778e00fa97fde9cea45295
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-09-20 10:01:57 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,11 +2,26 @@
 Handwritten code
 prints stars for how many characters are input
 """
-user_password = input("password: ")
-number_of_stars = len(user_password)
-while number_of_stars < 8:
-    print("Invalid password, too short")
+
+
+def main():
+    """This function prints stars for number of characters given"""
+    number_of_stars = get_password()
+    print_stars(number_of_stars)
+
+
+def print_stars(number_of_stars):
+    print(number_of_stars * "*")
+
+
+def get_password():
     user_password = input("password: ")
     number_of_stars = len(user_password)
+    while number_of_stars < 8:
+        print("Invalid password, too short")
+        user_password = input("password: ")
+        number_of_stars = len(user_password)
+    return number_of_stars
 
-print(number_of_stars * "*")
+
+main()
