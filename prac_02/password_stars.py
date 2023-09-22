17df1b418481758a8510a3f007f3fcfd8b19cca6
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-09-22 22:06:05 +1000

    Refactor functions to meet SRP requirements

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -18,17 +18,27 @@ def print_stars(number_of_stars):
 def get_password():
     """This function collects users password"""
     user_password = input("password: ")
-    number_of_stars = len(user_password)
+    number_of_stars = number_of_stars_length(user_password)
     return check_error(number_of_stars)
+
+
+def number_of_stars_length(user_password):
+    """This function counts the number of characters in user password"""
+    number_of_stars = len(user_password)
+    return number_of_stars
 
 
 def check_error(number_of_stars):
     """This function checks that the password is not shorter than eight characters"""
     while number_of_stars < 8:
-        print("Invalid password, too short")
-        user_password = input("password: ")
-        number_of_stars = len(user_password)
+        print_invalid()
+        get_password()
     return number_of_stars
+
+
+def print_invalid():
+    """This function prints invalid"""
+    print("Invalid password, too short")
 
 
 main()
