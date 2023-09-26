6ed68772f45d58657ca798d4089a8155f89cae70
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-09-27 09:04:45 +1000

    Add prac answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+"""
+CP1404 Practical
+Questions on random function
+"""
+
+# Smallest number possible is 5 and the largest possible is 20
+# Smallest number possible is 3 and the largest possible is 9. It's not possible to return 4
+# Smallest number possible is 2.5 and the largest possible is 5.5
+
+import random
+print(random.randint(1, 100))
