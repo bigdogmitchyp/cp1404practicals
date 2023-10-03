aa31b0d081fe88807f7fccc562d2777e66a79d99
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-10-04 09:42:39 +1000

    Add prac task

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Practical warm-up
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers [0] = 3
+# numbers [-1] = 2
+# numbers [3] = 1
+# numbers [:-1] = [3, 1, 4, 1 , 5, 9]
+# numbers [3:4] = 1
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(f"Is 9 an element of numbers? {9 in numbers}")
+print(numbers)
