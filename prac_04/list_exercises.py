5ea54216d8436ab1c1d250bd1ba2e81ed4dde6eb
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-10-05 09:05:21 +1000

    Add username task

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,11 @@ print(f"The last number is {numbers[4]:.0f}")
 print(f"The smallest number is {min(numbers):.0f} ")
 print(f"The largest number is {max(numbers):.0f}")
 print(f"The average of the numbers is {sum(numbers)/5:.1f}")
+
+usernames = ['jimbo,' 'giltson98', 'derekf', 'WhatSup', 'NicholEye', 'swei45', 'BaseInterpreterInterface',
+             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
