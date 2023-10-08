6c89252256ea6675384d3fd1621e12cfdfd611d1
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-10-08 15:39:26 +1000

    Refactor functions for SRP

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,16 +5,34 @@ import random
 
 
 def main():
-    number_of_picks = int(input("How many quick picks?: "))
+    """Print quick picks"""
+    number_of_picks = get_pick_number()
     for i in range(number_of_picks):
         random_number_row = []
-        for j in range(6):
-            number = random.randint(1, 45)
-            while number in random_number_row:
-                number = random.randint(1, 45)
-            random_number_row.append(number)
+        generate_random_picks(random_number_row)
         random_number_row.sort()
         print(" ".join(f"{number:2}" for number in random_number_row))
+
+
+def generate_random_picks(random_number_row):
+    """Generate random number lists"""
+    for j in range(6):
+        number = random.randint(1, 45)
+        number = check_if_number_valid(number, random_number_row)
+        random_number_row.append(number)
+
+
+def check_if_number_valid(number, random_number_row):
+    """Makes sure the number lists are without replacement"""
+    while number in random_number_row:
+        number = random.randint(1, 45)
+    return number
+
+
+def get_pick_number():
+    """Collect users number of picks"""
+    number_of_picks = int(input("How many quick picks?: "))
+    return number_of_picks
 
 
 main()
