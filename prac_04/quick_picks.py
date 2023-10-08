5e58c3070309133e43e9c7460f5f117cd4c834e4
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-10-08 11:12:24 +1000

    Add quick picks code

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,3 +1,15 @@
 """
 CP1404 Practical
 """
+import random
+
+
+def main():
+    number_of_picks = int(input("How many quick picks?: "))
+    for i in range(number_of_picks):
+        list_of_numbers = [random.sample(range(1, 46), 6)]
+        string_of_numbers = str(list_of_numbers).replace(", ", " ").strip("[]")
+        print(string_of_numbers)
+
+
+main()
