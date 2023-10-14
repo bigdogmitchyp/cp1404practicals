b4b881569c7b8a69963c6eb30f3946877f3c04cd
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-10-14 17:34:15 +1000

    Add exceptions and printing loop

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,10 +8,13 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+for code, state in CODE_TO_NAME.items():
+    print(f"{code:3} is {state}")
 state_code = input("Enter short state: ").upper()
+
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper
+    state_code = input("Enter short state: ").upper()
