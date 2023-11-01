ab9ce468201d9a71d7c6eb794b2e89ab315c5fa0
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-11-01 21:43:11 +1000

    Complete csv write function

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,36 +1,32 @@
 """
 CP1404 prac module
-est time = 14 min
-start time = 1314
-end time = 1320
-total time = 6 min
 """
 
 
 class Guitar:
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+        return f"{self.name}, {self.year}, {self.cost}"
+
+    def __iter__(self):
+        return self
 
     def __repr__(self):
-        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+        return f"{self.name}, {self.year}, {self.cost}"
 
     def __lt__(self, other):
         return self.year < other.year
-
-    def __iter__(self):
-        return [self.name, self.year, float(self.cost)]
 
     def get_age(self):
         guitar_age = 2023 - self.year
         return guitar_age
 
     def is_vintage(self):
-        if self.get_age() >= 50:
-            return True
-        else:
-            return False
+        return self.get_age() >= 50
+
+    def create_list(self):
+        return [self.name, self.year, self.cost]
--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,6 +4,7 @@ CP1404 Practical module
 
 import csv
 from guitar import Guitar
+from operator import itemgetter
 
 
 def main():
@@ -11,20 +12,26 @@ def main():
     reader = csv.reader(in_file)
     guitars = []
     for row in reader:
-        guitar = Guitar(row[0], row[1], float(row[2]))
+        guitar = Guitar((row[0]), int(row[1]), float(row[2]))
+        guitar = guitar.create_list()
         guitars.append(guitar)
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+    sorted_guitars = (sorted(guitars, key=itemgetter(1)))
+    for guitar in sorted_guitars:
+        print(f"{guitar[0]} ({guitar[1]}) : {guitar[2]:,.2f}")
     guitar_name = input("New guitar: ")
     while guitar_name != "":
         guitar_year = input("Year: ")
         guitar_cost = input("Price: ")
-        guitar = Guitar(guitar_name, guitar_year, float(guitar_cost))
+        guitar = Guitar(guitar_name, int(guitar_year), float(guitar_cost))
+        guitar = guitar.create_list()
         guitars.append(guitar)
-        for guitar in guitars:
-            print(guitar)
+        sorted_guitars = (sorted(guitars, key=itemgetter(1)))
+        for guitar in sorted_guitars:
+            print(f"{guitar[0]} ({guitar[1]}) : {guitar[2]:,.2f}")
         guitar_name = input("New guitar: ")
+    with open("guitars.csv", 'w+', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(guitars)
     in_file.close()
 
 
