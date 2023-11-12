32833de4299a34019be9734c47c43b0b2ffe8f74
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-11-12 21:31:28 +1000

    Complete handling invalid inputs

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -17,13 +17,19 @@ class MilesKiloConvertApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_miles(self, value):
-        result = float(value) * MILES_TO_KM
+    def handle_miles(self, text):
+        result = self.check_number(text) * MILES_TO_KM
         self.root.ids.output_number.text = str(result)
 
     def handle_increment(self, text, diff):
-        new_miles = float(text) + float(diff)
+        new_miles = self.check_number(text) + float(diff)
         self.root.ids.input_number.text = str(new_miles)
+
+    def check_number(self, text):
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 
 MilesKiloConvertApp().run()
