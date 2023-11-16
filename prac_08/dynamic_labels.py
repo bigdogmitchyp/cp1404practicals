535add70560c047c8c3f36d11c4b84775104a0e7
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-11-17 01:06:54 +1000

    Remove redundant init

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -6,12 +6,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
+name_list = ["alpha", "beta", "charlie", "delta", "echo"]
+
 
 class DynamicLabelsApp(App):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name_list = ["alpha", "beta", "charlie", "delta", "echo"]
 
     def build(self):
         self.title = "Dynamic Labels"
@@ -20,7 +18,7 @@ class DynamicLabelsApp(App):
         return self.root
 
     def create_widgets(self):
-        for name in self.name_list:
+        for name in name_list:
             temp_label = Label(text=name, font_size=48, color=(1, 1, 1, 1))
             self.root.ids.main.add_widget(temp_label)
 
