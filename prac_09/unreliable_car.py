c8b8a8521224b70b481a1696c9d52709febfaf51
bigdogmitchyp <mitchell.parker@my.jcu.edu.au>
2023-11-19 12:21:04 +1000

    Complete unreliable car test

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -7,7 +7,7 @@ import random
 
 class UnreliableCar(Car):
     """Class for a car which has a chance of failing to drive"""
-    def __init__(self, name, fuel, reliability):
+    def __init__(self, name, fuel, reliability=0.0):
         """Initialises an unreliable car instance, based on parent class Car"""
         super().__init__(name, fuel)
         self.reliability = reliability
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,10 @@
+"""
+CP1404 Prac
+"""
+
+from unreliable_car import UnreliableCar
+
+
+my_car = UnreliableCar("Bomb", 100, 50)
+my_car.drive(50)
+print(my_car)
